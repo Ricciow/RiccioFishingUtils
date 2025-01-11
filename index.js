1bfbbcfe31c88b68cf044dc239ab8f91ed49ffaa
Riccio <riccio.fernando@gmail.com>
2025-01-11 17:16:23 -0300

    Modified loading messages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-ChatLib.chat("&5[&b&lRFU&5] &6&lLoading!\n&cIf you dont see a loaded msg /ct reload ._.")
+ChatLib.chat("\n&5[&b&lRFU&5] &6&lLoading...\n&cIf you dont see a loaded msg /ct reload ._.\n")
 
 import "./features/party.js";
 import "./features/generalFishing.js";
@@ -15,4 +15,4 @@ import "./gui/gui.js";
 import "./utils/updatechecker.js";
 import "./settings/updateSettings.js";
 
-ChatLib.chat("&5[&b&lRFU&5] &2&lLoaded! \n&b&l/rfu &fto open settings\n&e&l/rfumove &fto move gui\n&9&l/rfudiscord &ffor official server")
+ChatLib.chat("\n&5[&b&lRFU&5] &2&lLoaded! \n&b&l/rfu &fto open settings\n&e&l/rfumove &fto move gui\n&9&l/rfudiscord &ffor official server\n")
