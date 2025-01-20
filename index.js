4624e932fc00c8eac71426c0222a93d63233138b
Riccio <riccio.fernando@gmail.com>
2025-01-20 11:29:02 -0300

    made autoupdate the last import

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 ChatLib.chat("\n&5[&b&lRFU&5] &6&lLoading...\n&cIf you dont see a loaded msg /ct reload ._.\n")
 
-import "./utils/update/autoupdate.js";
 import "./features/generalFishing.js";
 import "./features/chFishing.js";
 import "./features/other.js";
@@ -13,5 +12,6 @@ import "./utils/keybinds.js";
 import "./gui/gui.js";
 import "./settings/updateSettings.js";
 import "./features/chatCommands/other.js";
+import "./utils/update/autoupdate.js";
 
 ChatLib.chat("\n&5[&b&lRFU&5] &2&lLoaded! \n&b&l/rfu &fto open settings\n&e&l/rfumove &fto move gui\n&9&l/rfudiscord &ffor official server\n")
